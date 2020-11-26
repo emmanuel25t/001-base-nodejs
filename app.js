6277248e2ac12acd53fb3c560f6e16f78efd617d
Raul Emmanuel Garcia Delgadillo <190464@utags.edu.mx>
2020-11-26 16:33:08 -0600

    primer commit del proyecto con la multiplicacion completa

---
+++ app.js
@@ -0,0 +1,56 @@
+const argv = require('yargs')
+    .command('listar', 'imprime en pantalla la tabla de multiplicar', {
+        base:{
+            demand: true,
+            alias: 'b'
+        },
+        limite:{
+            alias: 'l',
+            default: 10
+        }
+
+    })
+    .command('crear', 'Crea un archivo con la tabla de multiplicar', {
+        base:{
+            demand: true,
+            alias: 'b'
+        },
+        limite:{
+            alias: 'l',
+            default: 10
+        }
+
+    })
+    .help()
+    .argv;
+const yargs = require('yargs');
+const multiplicar = require('./multiplicacion/multiplicacion');
+
+let comando = argv._[0];
+let base = argv.base;
+let limite = argv.limite;
+
+switch(comando) {
+    case `listar`:
+    
+    multiplicar.listarTabla(base, limite)
+    .then ()
+    .catch(error => console.log(`Ocurrio un error ${error}`));
+    break;
+    
+    case `crear`:
+    
+    multiplicar
+        .crearArchivo(base, limite)
+    .then(archivo => console.log(`El archivo ${archivo} ha sido generado con exito`))
+    .catch(error => console.log(`Ocurrio un error ${error}`));
+   
+    break;
+    default:
+    console.log(`Comando no reconocido`);
+}
+
+//console.log('La base insertada fue: ', argv.base);
+//console.log('El limite insertado fue: ', argv.limite);
+//console.log ('La opcion del menu seleccionado fue: ', argv._[0]);
+//console.log('informacion de la funcion: ', multiplicar);
